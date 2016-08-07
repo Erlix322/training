18305ed1048baae3d964064dad877cfce166943e
Eric Brandt <ericbrandt@gmx.de>
2016-08-07 18:12:50 +0200

    initial

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+//including libraries
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var data = require('./data/data.js');
+
+//loading server key and certificate to 
+//establish TLS connections later on
+var options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+};
+//initialize the express framework
+var app = express();
+https.createServer(options, app).listen(3000, function(){
+    console.log('Server started and listening on localhost:3000');
+});
+//apply logger to /user route
+app.use('/user',function(req,res,next){
+    console.log("Request on /user");
+    next();
+});
+
+app.get('/user',function (req,res) {
+    res.send(data.user);
+})
+
+app.get('/todo',function(req,res){
+    res.send(data.todo);
+})
+
+
+
+
+
+
+

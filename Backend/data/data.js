3339ff31d65e55aeaedaf75811d9ea2599e3ade2
Eric Brandt <ericbrandt@gmx.de>
2016-08-07 18:11:16 +0200

    initial

---
+++ Backend/data/data.js
@@ -0,0 +1,23 @@
+var User = [
+    {
+        id:1,
+        first:"Eric",
+        last :"Brandt", 
+    },
+    {
+        id:2,
+        first:"Felix",
+        last:"Brandt"
+    }
+]
+
+var Todo = [
+    {
+        id:1,
+        title:"Homework",
+        content:"I've got a lot todo today!"
+    }
+]
+
+exports.user = User;
+exports.todo = Todo;
